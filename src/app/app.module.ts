3021e4599409453d4afa1c98555dba317bfbd569
hcandelaria <hcandelaria66.7@gmail.com>
2018-07-10 03:22:21 -0400

    add routes. resume route complete. contact route updated. navbar opacity improved.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '',            component: HomeComponent},
+  { path: 'portfolio',   component: PortfolioComponent},
+  { path: 'resume',      component: ResumeComponent},
+  { path: 'contact',     component: ContactComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
